3172e3e52ae49fd8b55b9902f8abf3cfe5ac3c5b
Blake McMillan Shaw <blakeshaw@quickenloans.com>
2018-11-30 11:57:08 -0500

    initial commit

---
+++ comic-book-gallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,37 @@
+﻿using ComicBookGallery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ComicBookGallery.Data;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        private readonly ComicBookRepository _comicBookRepository = null;
+
+        public ComicBooksController()
+        {
+            _comicBookRepository = new ComicBookRepository();
+        }
+
+        public ActionResult Index()
+        {
+            var comicBooks = _comicBookRepository.GetComicBooks();
+
+            return View(comicBooks);
+        }
+        public ActionResult Detail(int? id)
+        {
+            if(id == null)
+            {
+                return HttpNotFound();
+            }
+            var comicBook = _comicBookRepository.GetComicBook((int)id);
+
+            return View(comicBook);
+        }
+    }
+}
