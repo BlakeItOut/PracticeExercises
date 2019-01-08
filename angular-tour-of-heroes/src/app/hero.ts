81a23201e12d49bd01e26370ac8ffcd854a1b8bb
Blake McMillan Shaw <blakeshaw@quickenloans.com>
2019-01-08 10:52:12 -0500

    Initial angular my-app

--- angular-tour-of-heroes/src/app/hero.ts
+++ angular-tour-of-heroes/src/app/hero.ts
@@ -1,4 +1,5 @@
 export class Hero {
     id: number;
+    url: string;
     name: string;
 }
--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
 
 @Component({
@@ -10,12 +9,6 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[]
-  
-  selectedHero: Hero
-
-  onSelect(hero: Hero): void {
-      this.selectedHero = hero
-  }
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
@@ -27,6 +20,18 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
 
 }
